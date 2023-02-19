967b951f34196d4397e1ca0050f66cb62c5b9f21
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-19 23:15:27 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,9 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+  //  <Layout>
       <Component {...pageProps} />
-    </Layout>
+  //  </Layout>
   );
 }
 
