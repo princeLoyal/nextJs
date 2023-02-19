77b12d36a6c11266e318ad8e4b33a2fe297ba3ce
princeLoyal <princeloyal1234@gmail.com>
2023-02-19 21:00:59 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,29 @@
-import { Fragment } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 const DummyList = [
-{ id: 'm1', title:'First meetup', 
-image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBYvzaFHCF7wRhGmE5UvaYIXIEgO-et0Q71w&usqp=CAU',
-address: 'Number 12, Akon road',
-},
+  {
+    id: 'm1', 
+    title:'First meetup', 
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBYvzaFHCF7wRhGmE5UvaYIXIEgO-et0Q71w&usqp=CAU',
+    address: 'Number 12, Akon road',
+    description: 'This is the first meetup',
+  },
+  {
+    id: 'm2', 
+    title:'Second meetup', 
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png',
+    address: 'Number 87, Akon road',
+    description: 'This is the second meetup',
+  },
+  {
+    id: 'm3', 
+    title:'Third meetup', 
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkQDIfOuSLBdsibcUHhT1TteMkO1VzJhGPfg&usqp=CAU',
+    address: 'Number 18, Akon road',
+    description: 'This is the third meetup',
+  },
 ];
+
 const HomePage = () => {
- return (
-   <Fragment>
-     <MeetupList meetups={DummyList}/>
-   </Fragment>
- );
+  return <MeetupList meetups={DummyList}/>
 }
 export default HomePage;
