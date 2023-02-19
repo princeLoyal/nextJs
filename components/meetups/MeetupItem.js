99751383d83911979cbba43063a5ccd6226ffc82
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-19 23:48:04 +0100

    Update MeetupItem.js

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -5,7 +5,7 @@ import classes from './MeetupItem.module.css';
 function MeetupItem(props) {
   const router = useRouter();
   function detailClickHandler(){
-    return router.push();
+    return router.push("/");
   }
   return (
     <li className={classes.item}>
