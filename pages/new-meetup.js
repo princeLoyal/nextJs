bd1b409393f9b6dc8a1ee1662dc827f865e19ba7
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-20 15:52:36 +0100

    Update new-meetup.js

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,19 +1,24 @@
 import { useRouter } from 'next/router';
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
   const router = useRouter();
+
   async function addMeetupHandler(enteredMeetupData) {
-    const response = await fetch('/api/new-meetup',{
+    const response = await fetch('/api/new-meetup', {
       method: 'POST',
       body: JSON.stringify(enteredMeetupData),
       headers: {
-        'Content-type' : 'application/json'
+        'Content-Type': 'application/json'
       }
     });
+
     const data = await response.json();
 
-    router('/');
+    console.log(data);
+
+    router.push('/');
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
