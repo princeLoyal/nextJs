57033a2246782d3d5749b2a99a3f8f988f3d5962
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-20 15:40:17 +0100

    Update new-meetup.js

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,8 +1,15 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    alert(enteredMeetupData);
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('/new-meetups',{
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-type' : 'application/json'
+      }
+    });
+    const data = await response.json();
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
