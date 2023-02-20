eb7e2a665e3efecfe766c022480d49a6b6f52370
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-20 13:40:24 +0100

    Update [meetupId].js

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,15 +1,28 @@
 
 import MeetupDetail from '../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
-      title='First Meetup'
-      address='Some Street 5, Some City'
-      description='This is a first meetup'
+      image = { props.meetup.image }
+      title = { props.meetup.title }
+      address = { props.meetup.address }
+      description = { props.meetup.description }
     />
   );
+}
+
+export function getStaticProps(){
+  return {
+     props: {
+       meetup: {
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+          title: 'First meetup',
+          address: 'Some Street 5, Some City',
+          description: 'This is the first meetup', 
+       },
+     },
+  };
 }
 
 export default MeetupDetails;
