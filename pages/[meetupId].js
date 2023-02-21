066a90be787f15d967b9e1152fef7e0c20e2426c
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-21 16:01:57 +0100

    Update [meetupId].js

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -39,10 +39,7 @@ export async function getStaticProps(context){
 
     const response = await fetch('https://nextjs-faf60-default-rtdb.firebaseio.com/meetups.json');
     const meetups = await response.json();
-    let meetup = {
-    title: 'id', 
-    address: 'usudud'
-    }
+    let meetup;
 
     for (const key in meetups){
       if(meetups[key].id === id){
