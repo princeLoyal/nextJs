fb5a186ecc517433819d8929da3d21bbe765346e
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-21 13:52:00 +0100

    Update new-meetup.js

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -6,7 +6,6 @@ function NewMeetupPage() {
   const router = useRouter();
 
   async function addMeetupHandler(enteredMeetupData) {
-alert('shhs')
     const response = await fetch('https://nextjs-faf60-default-rtdb.firebaseio.com/meetups.json', {
       method: 'POST',
       body: JSON.stringify(enteredMeetupData),
