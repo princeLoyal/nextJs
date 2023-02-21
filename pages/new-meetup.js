25a0b9b7a0ea46d81db032e153b09713f57659de
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-21 14:08:36 +0100

    Update new-meetup.js

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -8,18 +8,7 @@ function NewMeetupPage() {
 
   async function addMeetupHandler(enteredMeetupData) {
 alert('start')
-    const client = await MongoClient.connect(
-      'mongodb+srv://princeloyal1:clusterPrince@cluster0.vtkhsaw.mongodb.net/meetups?retryWrites=true&w=majority'
-    );
-    const db = client.db();
-
-    const meetupsCollection = db.collection('meetups');
-
-    const result = await meetupsCollection.insertOne(enteredMeetupData);
-
-    alert('reached');
-
-    client.close();
+    
 
     router.push('/');
   }
