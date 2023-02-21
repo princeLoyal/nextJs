64382b50d79796621d1e99c647c1d400eeb27b5b
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-21 14:06:21 +0100

    Update new-meetup.js

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { MongoClient } from 'mongodb';
 
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
@@ -6,13 +7,20 @@ function NewMeetupPage() {
   const router = useRouter();
 
   async function addMeetupHandler(enteredMeetupData) {
-    const response = await fetch('https://nextjs-faf60-default-rtdb.firebaseio.com/meetups.json', {
-      method: 'POST',
-      body: JSON.stringify(enteredMeetupData),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+alert('start')
+    const client = await MongoClient.connect(
+      'mongodb+srv://princeloyal1:clusterPrince@cluster0.vtkhsaw.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(enteredMeetupData);
+
+    alert('reached');
+
+    client.close();
+
     router.push('/');
   }
 
