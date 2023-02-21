aa8e842a32b001252eb4dd08b1a8ba7f44ff9d90
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-21 13:48:16 +0100

    Update new-meetup.js

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,26 +1,10 @@
-import { MongoClient } from 'mongodb';
+//import { MongoClient } from 'mongodb';
 
 // /api/new-meetup
 // POST /api/new-meetup
 
 async function handler(req, res) {
   alert('hshs')
-  if (req.method === 'POST') {
-    const data = req.body;
-
-    const client = await MongoClient.connect(
-      'mongodb+srv://princeloyal1:clusterPrince@cluster0.vtkhsaw.mongodb.net/meetups?retryWrites=true&w=majority'
-    );
-    const db = client.db();
-
-    const meetupsCollection = db.collection('meetups');
-
-    const result = await meetupsCollection.insertOne(data);
-
-    client.close();
-
-    res.status(201).json({ message: 'Meetup inserted!' });
-  }
 }
 
 export default handler;
